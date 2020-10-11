7e60baab1a60a249cdd376d01fef182ee422eccb
soyjoy666666 <soyjoy666666@users.noreply.github.com>
2020-10-11 06:46:09 +0000

    deploy: c9baa2d1c9393d0d33c8ef5c1e49367c992231e6

---
+++ precache-manifest.851d5a54ef2e3c61bc5581b766c7ee7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81c033ee1050bcee7a446197234e000",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a2b16f3bb03e553583f",
+    "url": "./static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "7969e83f3b178eb6143e",
+    "url": "./static/js/2.b06abf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b06abf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2b16f3bb03e553583f",
+    "url": "./static/js/main.796feb9d.chunk.js"
+  },
+  {
+    "revision": "03575165036da8444cf4",
+    "url": "./static/js/runtime-main.8f28b43c.js"
+  }
+]);
